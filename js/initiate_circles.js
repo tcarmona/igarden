60b82023ef0418079e846fbfbaef3173c7affc8b
Tiago Carmona <eu@tiagocarmona.com.br>
2017-05-28 17:17:14 -0300

    Adicionando a inicializacao dummy dos sensores

---
+++ js/initiate_circles.js
@@ -0,0 +1,48 @@
+var ph = Circles.create({
+          id:                  'circles-ph',
+          radius:              60,
+          value:               78,
+          maxValue:            100,
+          width:               10,
+          text:                function(value){return value + '%';},
+          colors:              ['#569532', '#1C4A00'],
+          duration:            400,
+          wrpClass:            'circles-wrp',
+          textClass:           'circles-text',
+          valueStrokeClass:    'circles-valueStroke',
+          maxValueStrokeClass: 'circles-maxValueStroke',
+          styleWrapper:        true,
+          styleText:           true
+});
+var humidity = Circles.create({
+          id:                  'circles-hum',
+          radius:              60,
+          value:               42,
+          maxValue:            100,
+          width:               10,
+          text:                function(value){return value + '%';},
+          colors:              ['#236267', '#012F34'],
+          duration:            400,
+          wrpClass:            'circles-wrp',
+          textClass:           'circles-text',
+          valueStrokeClass:    'circles-valueStroke',
+          maxValueStrokeClass: 'circles-maxValueStroke',
+          styleWrapper:        true,
+          styleText:           true
+});
+var lum = Circles.create({
+          id:                  'circles-lum',
+          radius:              60,
+          value:               66,
+          maxValue:            100,
+          width:               10,
+          text:                function(value){return value + '%';},
+          colors:              ['#AAA839', '#555300'],
+          duration:            400,
+          wrpClass:            'circles-wrp',
+          textClass:           'circles-text',
+          valueStrokeClass:    'circles-valueStroke',
+          maxValueStrokeClass: 'circles-maxValueStroke',
+          styleWrapper:        true,
+          styleText:           true
+});
