935c286a6febdb23e17bb4ac23467443eede7685
Tiago Carmona <tcarmona@cbitsgroup.com>
2017-06-26 15:18:15 -0300

    Inicia a bomba corretamente na requisição

--- app/main-code.py
+++ app/main-code.py
@@ -64,12 +64,12 @@ while True:
     # Calculate, using Thingspeak, the postion of our servo motor 
     servo_position = urllib.urlopen("https://api.thingspeak.com/channels/" + channel_id + "/fields/5/last.txt").read()
 #    print(servo_position)
-    if servo_position == "1" and current_position = 0:
+    if servo_position == "1" and current_position == 0:
         servo.start(7.5)
         current_position = 1
         time.sleep(3)
         servo.stop()
-    else and current_position != 0:
+    elif current_position != 0:
         servo.start(2.5)
         current_position = 0
         time.sleep(3)
@@ -77,11 +77,11 @@ while True:
     
     # See if we need to open the relay (water the plant)
     need_water = urllib.urlopen("https://api.thingspeak.com/channels/" + channel_id + "/fields/6/last.txt").read()
-    if need_water = "1":
+    if need_water == "1":
       update = urllib.urlopen("https://api.thingspeak.com/update?key=" + write_key + "&field6=0") 
-      GPIO.output(11, GPIO.LOW) # Start the bomb
+      GPIO.output(11, GPIO.LOW) # Start the pump
       time.sleep(5)
-      GPIO.output(11, GPIO.HIGH) # Stop the bomb
+      GPIO.output(11, GPIO.HIGH) # Stop the pump
         
     #Delay the processing until the next cycle
     time.sleep(delay)
