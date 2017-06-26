d93bee54e7be1f76103b5b1b939344dd2bead6ed
Tiago Carmona <tcarmona@cbitsgroup.com>
2017-06-26 11:09:43 -0300

    Usando pulsos de 3 segundos para o servo

--- app/main-code.py
+++ app/main-code.py
@@ -13,25 +13,27 @@ soil = 1   #Analog pin of the data cable of the soil moisture sensor
 write_key = "EFGX918WTOL8UZKT"
 read_key = "6UKWF6OLI9M3OUIQ"
 channel_id = "293365"
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7,GPIO.OUT)
 
+# Set the GPIO to use the Raspberry Port Numbers
+GPIO.setmode(GPIO.BOARD)
+
+# Configure the water pump relay 
 GPIO.setup(11, GPIO.OUT) 
 GPIO.output(11, GPIO.HIGH)
 
 
 # Define the servo interface
+GPIO.setup(7,GPIO.OUT)
 servo = GPIO.PWM(7,50)
 servo.start(2.5)
+time.sleep(3)
+servo.stop()
+current_position = 0
 
-# Create SPI
+# Create SPI, used for the analog sensors
 spi = spidev.SpiDev()
 spi.open(0, 0)
  
-# Connect with thingspeak magic
-channel = thingspeak.Channel(channel_id, read_key, write_key)
-
-
 def readadc(adcnum):
     # read SPI data from the MCP3008, 8 channels in total
     if adcnum > 7 or adcnum < 0:
@@ -55,7 +57,6 @@ while True:
 #    print("Soil Porcenture: %d" % soil_porc)
 
     # Send it to Thingspeak
-#    print(channel.get())
     update = urllib.urlopen("https://api.thingspeak.com/update?key=" + write_key + "&field1=" + str(light_porc) 
 + "&field2=" + str(light_value) + "&field3=" + str(soil_porc) + "&field4=" + str(soil_value))
 
@@ -63,18 +64,24 @@ while True:
     # Calculate, using Thingspeak, the postion of our servo motor 
     servo_position = urllib.urlopen("https://api.thingspeak.com/channels/" + channel_id + "/fields/5/last.txt").read()
 #    print(servo_position)
-    if servo_position == "1":
-        servo.ChangeDutyCycle(7.5)
-    else:
-        servo.ChangeDutyCycle(2.5)
+    if servo_position == "1" and current_position = 0:
+        servo.start(7.5)
+        current_position = 1
+        time.sleep(3)
+        servo.stop()
+    else and current_position != 0:
+        servo.start(2.5)
+        current_position = 0
+        time.sleep(3)
+        servo.stop()
     
     # See if we need to open the relay (water the plant)
     need_water = urllib.urlopen("https://api.thingspeak.com/channels/" + channel_id + "/fields/6/last.txt").read()
-#    if need_water = "1":
-    update = urllib.urlopen("https://api.thingspeak.com/update?key=" + write_key + "&field1=" + str(light_porc)) 
-    GPIO.output(11, GPIO.LOW) # Start the bomb
-    time.sleep(5)
-    GPIO.output(11, GPIO.HIGH) # Start the bomb
+    if need_water = "1":
+      update = urllib.urlopen("https://api.thingspeak.com/update?key=" + write_key + "&field6=0") 
+      GPIO.output(11, GPIO.LOW) # Start the bomb
+      time.sleep(5)
+      GPIO.output(11, GPIO.HIGH) # Stop the bomb
         
-
+    #Delay the processing until the next cycle
     time.sleep(delay)
